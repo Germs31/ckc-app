72f88ef33b6fcde9cb8051d884aeed404100feca
German Nunez <german90nunez@gmail.com>
2020-01-30 15:43:31 -0800

    add proper attributes to modal

--- components/card.vue
+++ components/card.vue
@@ -8,15 +8,13 @@
       height="200px"
       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
     >
-      <v-card-title>Top 10 Australian beaches</v-card-title>
+      <v-card-title>{{bookTitle}}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+    <v-card-subtitle class="pb-0">{{bookAuthor}}</v-card-subtitle>
 
     <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      {{bookDescription}}
     </v-card-text>
 
     <v-card-actions>
@@ -26,20 +24,13 @@
       >
         Share
       </v-btn>
-
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
 export default {
-
+  props: ["bookTitle", "bookAuthor", "bookDescription"]
 }
 </script>
 
--- components/modal.vue
+++ components/modal.vue
@@ -4,57 +4,31 @@
       <template v-slot:activator="{ on }">
         <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
       </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+        <v-card>
+            <v-card-title>
+                <span class="headline">Add Book</span>
+            </v-card-title>
+            
+            <v-card-text>
+                <v-container>
+                    <v-form>
+                        <v-text-field label="Name of Book *" required></v-text-field>
+                        <v-select v-model="category" :items="categories" label="Select A Category *"></v-select>
+                        <v-text-field v-model="title" label="Enter Book Title *"></v-text-field>
+                        <v-text-field v-model="author" label="Enter Book Author *"></v-text-field>
+                        <v-textarea v-model="description" label="Enter Book Description *"></v-textarea>
+                    </v-form>
+                    
+                </v-container>
+                <small>*indicates required field</small>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                    <v-btn @click.stop="saveBook" color="green">Add</v-btn>
+                    <v-btn color="green" text @click="dialog = false">Close</v-btn>
+            </v-card-actions>
+        </v-card>
     </v-dialog>
   </v-row>
 </template>
@@ -62,7 +36,14 @@
 <script>
     export default{
         data: () => ({
-            dialog: false
-        })
+            dialog: false,
+            catagory: "",
+            title: "",
+            author: "",
+            description: "",
+            categories: ["Recently read books", "Favorite books", "Best of the best"]
+        }),
+
     }
 </script>
+
