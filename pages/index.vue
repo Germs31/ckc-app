5ad497f5a5ad8bcfdf5953bdf35a70defe13be5d
German Nunez <german90nunez@gmail.com>
2020-01-30 16:29:00 -0800

    creat new book avialable

--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,91 @@
     <v-row>
       <v-col md="4">
         <h2 class="text-center mb-5">Recently Read Books</h2>
-        <Card />
+        <v-row v-for="(item,index) in recentBooks" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
+          >
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
       </v-col>
       <v-col md="4">
         <h2 class="text-center mb-5">Favourite Books</h2>
-        <Card />
+        <v-row v-for="(item,index) in favouriteBooks" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
+          >
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
       </v-col>
       <v-col md="4">
         <h2 class="text-center mb-5">Best of the Best</h2>
-        <Card />
+        <v-row v-for="(item,index) in bestOfTheBest" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
+          >
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
       </v-col>
     </v-row>
-    <Modal />
+    <BookModal />
   </div>
 </template>
 
 <script>
-import Card from '@/components/card.vue'
-import Modal from '@/components/modal.vue'
+import BookCard from "@/components/BookCard";
+import BookModal from "@/components/BookModal";
+import { eventBus } from "@/eventBus";
 export default {
-    components:{
-      Card,
-      Modal
-    }
-
-}
+  components: {
+    BookCard,
+    BookModal
+  },
+  data() {
+    return {
+      recentBooks: [],
+      favouriteBooks: [],
+      bestOfTheBest: []
+    };
+  },
+  created() {
+    eventBus.$on("save-book", cardData => {
+      if (cardData.category === "Recently read books") {
+        this.recentBooks.push(cardData);
+        this.recentBooks.sort((a, b) => b - a);
+      }
+      if (cardData.category === "Favourite books") {
+        this.favouriteBooks.push(cardData);
+        this.favouriteBooks.sort((a, b) => b - a);
+      }
+      if (cardData.category === "Best of the best") {
+        this.bestOfTheBest.push(cardData);
+        this.bestOfTheBest.sort((a, b) => b - a);
+      }
+    });
+  },
+  
+};
 </script>
+<style lang="scss" scoped>
+</style>
