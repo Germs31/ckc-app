444bfbe8a5f26ed7efc49abc5642db6555b8c4f4
German Nunez <german90nunez@gmail.com>
2020-01-30 20:34:45 -0800

    form validation complete

--- components/bookModal.vue
+++ components/bookModal.vue
@@ -2,12 +2,14 @@
   <v-dialog max-width="500px" v-model="open">
     <v-card class="p-5">
       <v-card-title>Add Books</v-card-title>
-      <v-form>
-        <v-select v-model="category" :items="categories" label="Select A Category"></v-select>
-        <v-text-field v-model="title" label="Enter Book Title"></v-text-field>
-        <v-text-field v-model="author" label="Enter Book Author"></v-text-field>
-        <v-textarea v-model="description" label="Enter Book Description"></v-textarea>
-      </v-form>
+      <v-container>
+        <v-form ref="form" v-model="valid">
+          <v-select v-model="category" :items="categories" label="Select A Category" :rules="catRules" required></v-select>
+          <v-text-field v-model="title" label="Enter Book Title" :rules="titleRules" required></v-text-field>
+          <v-text-field v-model="author" label="Enter Book Author" :rules="authorRules" required></v-text-field>
+          <v-textarea v-model="description" label="Enter Book Description" :rules="desRules" required></v-textarea>
+        </v-form>
+      </v-container>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn @click.stop="saveBook" color="green">Add</v-btn>
@@ -26,7 +28,19 @@ export default {
       title: "",
       author: "",
       description: "",
-      categories: ["Recently read books", "Favourite books", "Best of the best"]
+      categories: ["Recently read books", "Favourite books", "Best of the best"],
+      catRules: [
+        v => !!v || 'Catagory is required',
+      ],
+      titleRules: [
+        v => !!v || 'Title is required',
+      ],
+      authorRules: [
+        v => !!v || 'Author is required',
+      ],
+      desRules: [
+        v => !!v || 'Description is required',
+      ]
     };
   },
   created() {
@@ -42,14 +56,25 @@ export default {
   },
   methods: {
     saveBook() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
       let cardData = {
         title: this.title,
         author: this.author,
         description: this.description,
         category: this.category
       };
+      if( cardData.title == '' || 
+          cardData.author == '' || 
+          cardData.description == '' || 
+          cardData.category == '' || 
+          cardData == ''){
+        return this.open=true;
+      }
       eventBus.$emit("save-book", cardData);
       this.open = false;
+      this.$refs.form.reset();
     }
   }
 };
